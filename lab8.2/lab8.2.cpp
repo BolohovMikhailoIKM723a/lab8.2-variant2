229864fd4cd2f3caa59d14eec0f954f22570bfb2
Михаил <@DESKTOP-1RCLF6R>
2024-03-16 23:02:54 +0200

    Add project files.

---
+++ lab8.2/lab8.2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "ukr");
+    int n;
+    cout << "введiть розмiр масиву: ";
+    cin >> n;
+    double* arr = new double[n];
+    cout << "введiть " << n << " дiйсних чисел:\n";
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    double product = 1.0;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] > 0) {
+            product *= arr[i];
+        }
+    }
+    cout << "добуток додатних елементiв масиву: " << product << endl;
+    double minElement = arr[0];
+    int minIndex = 0;
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] < minElement) {
+            minElement = arr[i];
+            minIndex = i;
+        }
+    }
+    double sum = 0.0;
+    for (int i = 0; i < minIndex; ++i) {
+        sum += arr[i];
+    }
+    cout << "сума елементiв масиву, розташованих до мiнiмального елемента: " << sum << endl;
+    delete[] arr;
+
+    return 0;
+}
